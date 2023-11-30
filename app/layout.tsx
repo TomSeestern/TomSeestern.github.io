ead1fe432c8729814d11126153c2bfd48c005e14
Tom Segbers <tomsegbers@googlemail.com>
2023-11-30 10:33:44 +0100

    Import footer and Header and add them to the layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,18 @@
 import "styles/tailwind.css"
+import React from "react"
+import { Header } from "../components/Header/Header"
+import { Footer } from "../components/Footer/Footer"
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="flex min-h-screen flex-col">
+        <Header />
+
+        <main className="flex-grow bg-gray-100 dark:bg-gray-900">{children}</main>
+
+        <Footer />
+      </body>
     </html>
   )
 }
