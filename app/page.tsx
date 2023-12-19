dc99f196ede9b63e0ae32dc4342078cba1705b70
Tom Segbers <tomsegbers@googlemail.com>
2023-12-19 15:03:12 +0100

    Update: Texts on Landingpage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import { Metadata } from "next"
-import Image from "next/image"
 import React from "react"
 import path from "path"
 import fs from "fs"
@@ -58,65 +57,46 @@ export default function Web() {
         <div className="mx-auto max-w-screen-xl gap-8 px-4 py-8 lg:py-16 xl:grid xl:grid-cols-12">
           <div className="col-span-8">
             <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
-              Discover new product and best possibilities
+              Exploring Innovation and Creativity in Technology
             </h1>
             <p className="mb-6 font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
-              Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value
-              and drive economic growth.
+              Welcome to my digital space where I share insights and developments on my latest tech projects and
+              thought-provoking articles on various tech topics.
             </p>
-            <div className="items-center gap-16 sm:flex">
-              <div className="mb-8 text-gray-500 dark:text-gray-400 sm:mb-0">
-                <Image
-                  className="mb-3 h-7 w-7 rounded-full"
-                  src="/icon/calender.svg"
-                  alt="Calender Icon"
-                  width={512}
-                  height={512}
-                />
-
-                <h2 className="mb-3 text-xl font-semibold text-gray-900 dark:text-white">28 November 2021</h2>
-                <p className="mb-4 font-light">
-                  Join us at FlowBite 2021 to understand what’s next as the global tech and startup ecosystem, rethinks
-                  the future of everything.
+            <div className="flex flex-col items-center gap-8 sm:flex-row">
+              {/* Project Section */}
+              <div className="mb-8 flex-1 text-gray-500 dark:text-gray-400 sm:mb-0">
+                {/* TODO: Replace with your project image and details */}
+                <h2 className="mb-3 text-xl font-semibold text-gray-900 dark:text-white">Latest Project: LAYZR.gg</h2>
+                <p className="mb-4 line-clamp-3 min-h-[4.5rem] font-light ">
+                  Dive into the details of my recent project, LAYZR.gg, where I explore digital collectibles and the
+                  unique digital identity they provide.
                 </p>
                 <a
-                  href="#"
+                  href="/projects"
                   className="inline-flex items-center rounded-lg bg-primary-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                 >
-                  Conference
-                  <Image
-                    className="-mr-1 ml-2 h-5 w-5"
-                    src="/icon/arrow-right.svg"
-                    alt="Arrow right Icon"
-                    width={512}
-                    height={512}
-                  />
+                  Learn More
                 </a>
               </div>
-              <div className="text-gray-500 dark:text-gray-400">
-                <Image
-                  className="mb-3 h-7 w-7 rounded-full"
-                  src="/icon/speaker.svg"
-                  alt="Speaker Icon"
-                  width={512}
-                  height={512}
-                />
-                <h2 className="mb-3 text-xl font-semibold text-gray-900 dark:text-white">25+ top notch speakers</h2>
-                <p className="mb-4 font-light">
-                  Here you will find keynote speakers, who all are able to talk about Recruiting. Click on the
-                  individual keynote speakers and read more about them and their keynotes.
+              {/* Blog Section */}
+              <div className="mb-0 flex-1 text-gray-500 dark:text-gray-400">
+                <h2 className="mb-3 text-xl font-semibold text-gray-900 dark:text-white">Recent Blog Posts</h2>
+                <p className="mb-4 line-clamp-3 min-h-[4.5rem] font-light ">
+                  Explore my latest thoughts and insights on Tech, AI and sometimes a bit about software development.
+                  Mostly lessons learned from my projects.
                 </p>
                 <a
-                  href="#"
+                  href="[Blog Link]"
                   className="inline-flex items-center rounded-lg border border-gray-300 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
                 >
-                  <Image className="-ml-1 mr-2 h-5 w-5" src="/icon/list.svg" alt="List Icon" width={512} height={512} />
-                  View list
+                  Read the Blog
                 </a>
               </div>
             </div>
           </div>
           <div className="absolute right-0 top-0 hidden h-full w-1/3 xl:block">
+            {/* TODO: Replace with a relevant personal image*/}
             <img
               className="h-full w-full object-cover"
               src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/conference-speaker.jpg"
@@ -170,13 +150,13 @@ export default function Web() {
         <div className="mx-auto gap-8 px-4 py-8 lg:grid-cols-2 lg:gap-16 lg:px-6 lg:py-16 ">
           <div>
             <h2 className="mb-4 text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white">
-              My Recent Projects
+              My Recent Blog Posts
             </h2>
             <p className="font-light text-gray-500 dark:text-gray-400 sm:text-xl">
-              We use an agile approach to test assumptions and connect with the needs of your audience early and often.
+              These are some of my recent Blog Posts. I write about a variety of topics, including AI, Tech, and more.
             </p>
           </div>
-          <div className="divide-y divide-gray-200 dark:divide-gray-700">
+          <div className="my-8 divide-y divide-gray-200 dark:divide-gray-700">
             {articles.slice(0, 3).map((article, index) => (
               <ArticleComponent
                 key={article.id}
