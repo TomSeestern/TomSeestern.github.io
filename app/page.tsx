27ae1e37e0058f94806e5e5653dad3e764422cee
Tom Segbers <tomsegbers@googlemail.com>
2023-12-19 15:27:44 +0100

    Update: Texts on Landingpage

--- app/page.tsx
+++ app/page.tsx
@@ -87,7 +87,7 @@ export default function Web() {
                   Mostly lessons learned from my projects.
                 </p>
                 <a
-                  href="[Blog Link]"
+                  href="/blog"
                   className="inline-flex items-center rounded-lg border border-gray-300 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
                 >
                   Read the Blog
