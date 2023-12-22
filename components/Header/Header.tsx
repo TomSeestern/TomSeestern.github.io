d74cbd967282748144530b39d2a56945dccef94d
Tom Segbers <tomsegbers@googlemail.com>
2023-12-22 11:27:55 +0100

    feat: Replace normal Header with flowbite-react integrated one

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,95 +1,35 @@
-import React from "react"
+import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react"
 import Image from "next/image"
+import React from "react"
 
-export interface HeaderProps {
-  //    underline?: boolean
-  //    href: string
-}
-
-export function Header({ ...props }: HeaderProps) {
+export function Header() {
   return (
-    <header>
-      <nav className="border-gray-200 bg-white px-4 py-2.5 dark:bg-gray-800 lg:px-6">
-        <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between">
-          <a href="/" className="flex items-center">
-            <Image
-              src="/img/logo.png"
-              className="mr-3 h-6 w-6 sm:h-9 sm:w-9"
-              alt="Tom Segbers Logo"
-              width={512}
-              height={512}
-            />
-            <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Tom Segbers</span>
-          </a>
-          <div className="flex items-center lg:order-2">
-            <a
-              href="/contact"
-              className="mr-2 rounded-lg bg-primary-700 px-4 py-2 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 lg:px-5 lg:py-2.5"
-            >
-              Contact
-            </a>
-            <button
-              data-collapse-toggle="mobile-menu-2"
-              type="button"
-              className="ml-1 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 lg:hidden"
-              aria-controls="mobile-menu-2"
-              aria-expanded="false"
-            >
-              <span className="sr-only">Open main menu</span>
-              <Image
-                className={"h-6 w-6"}
-                src={"/icon/burger-menu.svg"}
-                alt="Burger Menu Icon"
-                width={32}
-                height={32}
-              />
-              <Image
-                className={"hidden h-6 w-6"}
-                src={"/icon/close-x.svg"}
-                alt="Close Menu Icon"
-                width={32}
-                height={32}
-              />
-            </button>
-          </div>
-          <div className="hidden w-full items-center justify-between lg:order-1 lg:flex lg:w-auto" id="mobile-menu-2">
-            <ul className="mt-4 flex flex-col font-medium lg:mt-0 lg:flex-row lg:space-x-8">
-              <li>
-                <a
-                  href="about"
-                  className="block border-b border-gray-100 py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white lg:border-0 lg:p-0 lg:hover:bg-transparent lg:hover:text-primary-700 lg:dark:hover:bg-transparent lg:dark:hover:text-white"
-                >
-                  About
-                </a>
-              </li>
-              <li>
-                <a
-                  href="projects"
-                  className="block border-b border-gray-100 py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white lg:border-0 lg:p-0 lg:hover:bg-transparent lg:hover:text-primary-700 lg:dark:hover:bg-transparent lg:dark:hover:text-white"
-                >
-                  Projects
-                </a>
-              </li>
-              <li>
-                <a
-                  href="blog"
-                  className="block border-b border-gray-100 py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white lg:border-0 lg:p-0 lg:hover:bg-transparent lg:hover:text-primary-700 lg:dark:hover:bg-transparent lg:dark:hover:text-white"
-                >
-                  Blog
-                </a>
-              </li>
-              <li>
-                <a
-                  href="contact"
-                  className="block border-b border-gray-100 py-2 pl-3 pr-4 text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white lg:border-0 lg:p-0 lg:hover:bg-transparent lg:hover:text-primary-700 lg:dark:hover:bg-transparent lg:dark:hover:text-white"
-                >
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
+    <Navbar fluid={true} rounded={true}>
+      <NavbarBrand href="/">
+        <Image
+          src="/img/logo.png"
+          className="mr-3 h-6 w-6 sm:h-9 sm:w-9"
+          alt="Tom Segbers Logo"
+          width={512}
+          height={512}
+        />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Tom Segbers</span>
+      </NavbarBrand>
+      <div className="flex md:order-2">
+        <a
+          href="/contact"
+          className="mr-2 rounded-lg bg-primary-700 px-4 py-2 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 lg:px-5 lg:py-2.5"
+        >
+          Contact
+        </a>
+        <NavbarToggle />
+      </div>
+      <NavbarCollapse>
+        <NavbarLink href="about">About</NavbarLink>
+        <NavbarLink href="projects">Projects</NavbarLink>
+        <NavbarLink href="blog">Blog</NavbarLink>
+        <NavbarLink href="contact">Contact</NavbarLink>
+      </NavbarCollapse>
+    </Navbar>
   )
 }
