9b92e0e8d750ef8e0b70da7715b2d2b2ac254e1b
Tom Segbers <tomsegbers@googlemail.com>
2023-12-22 12:05:39 +0100

    feat: Center texts on Project overview page so the look is same on every page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,9 @@
-import path from "path"
-import fs from "fs"
 import matter from "gray-matter"
-import ProjectTeaser from "../../components/ProjectTeaser/ProjectTeaser"
 import Image from "next/image"
 import React from "react"
+import fs from "fs"
+import path from "path"
+import ProjectTeaser from "../../components/ProjectTeaser/ProjectTeaser"
 
 function Articles() {
   const postsDirectory = path.join(process.cwd(), "app/blog/entry/")
@@ -30,6 +30,8 @@ function Articles() {
           fullArticleLink: "/blog/entry/" + filename.replace(/\.md?$/, ""),
           technologies: data.technologies || [],
         }
+      } else {
+        return null
       }
     })
     .filter(Boolean) // Filter out undefined values (directories)
@@ -37,21 +39,20 @@ function Articles() {
 
   return (
     <>
-      <section className="max-w-2xl space-y-6 bg-white  antialiased dark:bg-gray-900">
-        <div>
-          <p className="text-lg font-medium leading-none text-primary-600 dark:text-primary-500">Trusted Worldwide</p>
-          <h2 className="mt-3 text-3xl font-extrabold leading-tight tracking-tight text-gray-900 dark:text-white sm:text-4xl">
-            Trusted by over 100 companies and 10,000+ freelancers
+      <section className="mx-auto max-w-2xl space-y-6 bg-white antialiased dark:bg-gray-900">
+        <div className="mx-auto max-w-screen-md px-4 pt-8 lg:pt-16">
+          <h2 className="mt-3 text-center text-3xl font-extrabold leading-tight tracking-tight text-gray-900 dark:text-white sm:text-4xl">
+            Showcasing My Journey in Technology and Innovation
           </h2>
-          <p className="mt-4 text-base font-normal text-gray-500 dark:text-gray-400 sm:text-xl">
-            Our rigorous security and compliance standards are at the heart of all we do. We work tirelessly to protect
-            you and your customers.
+          <p className="mt-4 text-center text-base font-normal text-gray-500 dark:text-gray-400 sm:text-xl">
+            Each project represents a unique blend of creativity, technical skill, and dedication. Discover how I
+            approach challenges and innovate solutions.
           </p>
         </div>
-        <div className="space-y-4">
+        <div className="flex justify-center space-x-4">
           <a
-            href="#"
-            title=""
+            href="/projects"
+            title="View all projects"
             className="flex items-center text-base font-medium text-primary-600 hover:underline dark:text-primary-500"
           >
             View all projects
@@ -64,11 +65,11 @@ function Articles() {
             />
           </a>
           <a
-            href="#"
-            title=""
+            href="/blog"
+            title="View all Blog Posts"
             className="flex items-center text-base font-medium text-primary-600 hover:underline dark:text-primary-500"
           >
-            View all testimonials
+            View Blog posts
             <Image
               className={"ml-2 h-4 w-4"}
               src={"/icon/arrow-right.svg"}
@@ -83,7 +84,7 @@ function Articles() {
       <section className="bg-white antialiased dark:bg-gray-900">
         {/* Grid of 3x3 cards, 1 collum on mobile*/}
         <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-          {articles.map((article, index) => (
+          {articles.map((article) => (
             <ProjectTeaser
               key={article.id}
               title={article.title}
