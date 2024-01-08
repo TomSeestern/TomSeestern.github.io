be850547b5062b4d65cb2737768c6b231e8ffd94
Tom Segbers <tomsegbers@googlemail.com>
2024-01-08 18:25:35 +0100

    Update texts on About page, add Hattec logo

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,8 @@
 import path from "path"
 import TimelineEntry from "../../components/TimelineEntry/TimelineEntry"
 import PersonTeaser from "../../components/PersonTeaser/PersonTeaser"
+import Image from "next/image"
+import React from "react"
 
 export default function About() {
   return (
@@ -11,7 +13,7 @@ export default function About() {
             About me
           </h2>
           <p className="text-xl font-medium leading-tight text-gray-500 dark:text-gray-400">
-            A short overview about my recent Projects and Achievements
+            A short overview about my recent Formal Education and Work Experience.
           </p>
           <span className="inline-flex items-center rounded bg-primary-100 px-2.5 py-0.5 text-xs font-medium text-primary-800 dark:bg-primary-900 dark:text-primary-300">
             <svg
@@ -27,17 +29,17 @@ export default function About() {
                 clipRule="evenodd"
               />
             </svg>
-            Last updated on 03.01.2023
+            Last updated on 09.01.2023
           </span>
         </div>
         <div id="timeline" className="mt-12 grid grid-cols-1 gap-x-16 gap-y-12 lg:mt-16">
           <div className="space-y-8">
-            <h3 className="text-center text-2xl font-bold text-gray-900 dark:text-white">Positions:</h3>
+            <h3 className="text-center text-2xl font-bold text-gray-900 dark:text-white">Formal Positions:</h3>
             <div>
               <TimelineEntry
                 time="2021 - 2024"
                 title="Founder & Tech Lead @ LAYZR.gg"
-                description="Founding a new startup in the gaming industry. "
+                description="Founding a new startup at the intersection of streaming and blockchain."
                 link="https://layzr.gg"
               >
                 <PersonTeaser
@@ -49,46 +51,28 @@ export default function About() {
               </TimelineEntry>
               <TimelineEntry
                 time="2020-2021"
-                title="HATtec: Software Engineer Intern"
-                description="Working as a Software Engineer Intern at HATtec GmbH."
+                title="HATtec: Bachelor and Working Student"
+                description="Focusing on developing real-time AI solutions for my Bachelor Thesis."
                 link="https://hattec.de"
               >
                 <PersonTeaser
                   companyName="HATtec GmbH"
-                  companyLogoSrc="https://placehold.co/512x512?text=HATtec"
+                  companyLogoSrc="/img/hattec_logo.jpg"
                   companyDescription="Software Company developing autonomous Drone Software"
                   companyLink="https://hattec.de"
                 />
               </TimelineEntry>
               <TimelineEntry
                 time="2018-2021"
-                title="B.A. Applied Computer Science"
-                description=""
+                title="(B.SC.) Applied Computer Science"
+                description="Completed my Bachelor of Applied Computer Science at the Ravensburg-Weingarten University of Applied
+                  Science."
                 link="https://rwu.de"
               >
                 <div className="space-y-4 rounded-lg bg-gray-100 p-4 dark:bg-gray-800">
-                  <p className="text-base font-medium text-gray-500 dark:text-gray-400">Sponsors:</p>
-                  <div className="mt-2 flex flex-wrap items-center gap-x-6 gap-y-4">
-                    <img
-                      className="h-7 w-auto object-contain"
-                      src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/customers/spotify-grayscale.svg"
-                      alt=""
-                    />
-                    <img
-                      className="h-5 w-auto object-contain"
-                      src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/customers/microsoft-grayscale.svg"
-                      alt=""
-                    />
-                    <img
-                      className="h-5 w-auto object-contain"
-                      src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/customers/slack-grayscale.svg"
-                      alt=""
-                    />
-                    <img
-                      className="h-7 w-auto object-contain"
-                      src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/customers/salesforce-grayscale.svg"
-                      alt=""
-                    />
+                  <div className="flex flex-col items-start text-base font-medium text-gray-500 dark:text-gray-400">
+                    <p> - Specialization in robotics and AI. </p>
+                    <p> - Additional subjects in the areas of Deep Learning and Project Management. </p>
                   </div>
                 </div>
               </TimelineEntry>
@@ -104,6 +88,19 @@ export default function About() {
                   companyDescription="See Projects for more details"
                   companyLink="/projects"
                 />
+
+                <div className="flex flex-row items-center gap-2 text-base font-medium text-gray-500 hover:underline dark:text-gray-400">
+                  <a href="/projects" className="">
+                    See Projects
+                  </a>
+                  <Image
+                    className={"h-4 w-4"}
+                    src={"/icon/arrow-right.svg"}
+                    alt="arrow right"
+                    width={32}
+                    height={32}
+                  ></Image>
+                </div>
               </TimelineEntry>
             </div>
           </div>
